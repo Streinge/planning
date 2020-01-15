75a0e63db27fd0de3a1f12104dcf58411a9a33a7
Streinge <olesof@mail.ru>
2020-01-15 22:32:48 +0500

    15.01.20

--- planning/scripts/planning.py
+++ planning/scripts/planning.py
@@ -2,28 +2,36 @@
 #coding:utf-8
 from tkinter import *
 from tkinter import messagebox
-
+# splashscreen
 def main():
     root = Tk()
-    """c = Canvas(root, width=200, height=200, bg='white')
-    c.pack()
- 
-    c.create_line(10, 10, 190, 50)
- 
-    c.create_line(100, 180, 100, 60, fill='green',
-                width=5, arrow=LAST, dash=(10,2),
-                activefill='lightgreen',
-                arrowshape="10 20 10")"""
-    root.geometry('500x350+300+200')
-    root['bg'] = 'green'
+    image = PhotoImage(file='/home/streinge/planning/planning/image/photo.png')
+    button=Button(root, image=image)
+    button.pack()
+   
+    #Возвращает разрешение экрана по ширине в пикселях.
+    ws = root.winfo_screenwidth() 
+    #Возвращает разрешение экрана по высоте в пикселях.
+    hs = root.winfo_screenheight()
+    w = 500 #root.winfo_width()
+    h = 300 #root.winfo_height()
+    x = (ws/2) - (w/2)
+    y = (hs/2) - (h/2)
+
+    root.geometry('%dx%d+%d+%d' % (w, h, x, y))
+    
     root.overrideredirect(True)
-    #Label(a, text="About this").pack(expand=1)
+    
     root.after(3000, lambda: root.destroy())
     root.mainloop()
+    
+    print(w)
+    print(h)
+
    
 
 
-
+    # root['bg'] = 'green' изменение фона окна на зеленый
 if __name__ == '__main__':
     main()
 
