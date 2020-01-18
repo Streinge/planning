8324990d887a7d641bc010422947574cd2448524
Streinge <olesof@mail.ru>
2020-01-18 15:58:27 +0500

    18.01.2020

--- planning/scripts/planning.py
+++ planning/scripts/planning.py
@@ -1,38 +1,6 @@
 #!/usr/bin/env python3
 #coding:utf-8
-from tkinter import *
-    # функция принимает строку с путем к файлу заставки и  создает заставку к программе
-def splashscreen(path_to_image):
-    # класс Тк создаетс базовое окно заставки
-    splashscreen = Tk()
-    # класс PhotoImage позволяет использовать полноцветное изображение
-    image = PhotoImage(file=path_to_image)
-    # для создания фона с фото используем виджет "Кнопка" без указания размера, поэтому кнопка получилось со все окно и заполнила фон
-    button=Button(splashscreen,text='Плани', image=image)
-    lab=Label(splashscreen,text='Плани',fg='red',font='arial 14')
-    # упаковываем кнопку упаковщиком pack
-    button.pack()
-    lab.pack()
-    #Возвращает разрешение экрана по ширине в пикселях.
-    ws = splashscreen.winfo_screenwidth() 
-    #Возвращает разрешение экрана по высоте в пикселях.
-    hs = splashscreen.winfo_screenheight()
-    #Задаем ширину окна заставки
-    width = 600
-    #Задаем высоту окна заставки
-    height = 500
-    #Определяем координат для размещения заставки точно по центру экрана
-    x = (ws/2) - (width/2)
-    y = (hs/2) - (height/2)
-    #задаем положение и размеры окна заставки
-    splashscreen.geometry('%dx%d+%d+%d' % (width, height, x, y))
-    #этим методом игнорируем окно заголовка окна, чтобы заставка выглядела, как заставка.
-    splashscreen.overrideredirect(True)
-    splashscreen.after(3000, lambda: splashscreen.destroy())
-    splashscreen.mainloop()
-
-
-
+from splashscreen import splashscreen
 def main():
     splashscreen('/home/streinge/planning/planning/image/photo.png')
 if __name__ == '__main__':
--- planning/scripts/splashscreen.py
+++ planning/scripts/splashscreen.py
@@ -1,29 +1,56 @@
 from tkinter import *
-from time import sleep
-# функция принимает строку с путем к файлу заставки и  создает заставку к программе
-def move_char(x, y):
-    print("move")
-    splashscreen.move(text, x, y)
-    main.after(50, move_char(x, y))
+
+# функция принимает строку с путем к файлу заставки и  создает заставку к приложению
+def move_char():
+    global x
+    if x <=0:
+        return
+    else:
+        x += 0.1
+        splashscreen.move(text, -5, 0)
+        main.after(10, lambda: move_char())
 
 def splashscreen(path_to_image):
+    global main, splashscreen
+    global text, x
     # класс Тк создает базовое окно заставки
     main = Tk()
+    #Задаем ширину окна заставки
+    width = 700
+    #Задаем высоту окна заставки
+    height = 467
+
     # создаем холст внутри окна - это и будет заставка
-    splashscreen = Canvas(main,height=250, width=300)
+    splashscreen = Canvas(main,height=height, width=width)
     # класс PhotoImage позволяет использовать полноцветное изображение
     filename = PhotoImage(file=path_to_image)
-    # рисуем изображение из файла на холсте
-    image = splashscreen.create_image(300, 0, anchor=NE, image=filename)
-    text = splashscreen.create_text(20 , 150, text = 'П', font = 'Arial 72')
+    # рисуем изображение из файла на холсте, метод create_image принимает координаты точки отсчета для расположения
+    # фотографиии - это первые два значения, потом еще разные аргументы но важный это achor - указатель куда 
+    # привязать координаты точки отсчета, в моем случае поставил anchor=CENTER, то есть изображение будет центрированно
+    # по точке отсчета. Дальше также метод принимает значение метода PhotoImage.
+    image = splashscreen.create_image(width / 2, height / 2, anchor=CENTER, image=filename)
+    x = 0
+    text = splashscreen.create_text(width, 150, text = 'Планирование', anchor=NW,  font = 'Times 72', fill = '#2373d9')
+
+    #Возвращает разрешение экрана по ширине в пикселях.
+    ws = main.winfo_screenwidth() 
+    #Возвращает разрешение экрана по высоте в пикселях.
+    hs = main.winfo_screenheight()
+    #Определяем координат для размещения заставки точно по центру экрана
+    x = (ws/2) - (width/2)
+    y = (hs/2) - (height/2)
+    #задаем положение и размеры окна заставки
+    main.geometry('%dx%d+%d+%d' % (width, height, x, y))
+    #этим методом игнорируем окно заголовка окна, чтобы заставка выглядела, как заставка.
+    main.overrideredirect(True)
+    main.after(5000, lambda: main.destroy())
+
+
+
+
+     # упаковываем кнопку упаковщиком pack
     splashscreen.pack()
-    
-    main.after(2000, lambda: main.destroy())
-
-    # main.after(500, lambda: splashscreen.delete(text))
-    # упаковываем кнопку упаковщиком pack
-    move_char(1, 0)
+    move_char()
     main.mainloop()
-
 
 splashscreen('/home/streinge/planning/planning/image/photo.png')
